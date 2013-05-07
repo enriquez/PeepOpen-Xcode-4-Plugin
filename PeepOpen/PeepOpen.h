3cecb748e30885ff2e869424b0283a15b14f334e
Mike Enriquez <mike@enriquez.me>
2013-05-07 19:14:24 -0400

    FIRST

---
+++ PeepOpen/PeepOpen.h
@@ -0,0 +1,13 @@
+//
+//  PeepOpen.h
+//  PeepOpen
+//
+//  Created by Michael Enriquez on 5/6/13.
+//  Copyright (c) 2013 Mike Enriquez. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface PeepOpen : NSObject
+
+@end
---
+++ PeepOpen/PeepOpen.m
@@ -0,0 +1,73 @@
+//
+//  PeepOpen.m
+//  PeepOpen
+//
+//  Created by Michael Enriquez on 5/6/13.
+//  Copyright (c) 2013 Mike Enriquez. All rights reserved.
+//
+
+#import "PeepOpen.h"
+
+@class IDEWorkspaceWindowController;
+
+@interface NSObject()
++ (id)workspaceWindowControllers;
++ (id)lastActiveWorkspaceWindow;
+@end
+
+@interface PeepOpen ()
+- (NSURL *)projectRoot;
+@end
+
+@implementation PeepOpen
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedPlugin = [[self alloc] init];
+    });
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        NSMenuItem *fileMenuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        
+        if (fileMenuItem) {
+            NSMenuItem *open = [[NSMenuItem alloc] initWithTitle:@"Open with PeepOpen…" action:@selector(openWithPeepOpen) keyEquivalent:@"o"];
+            NSInteger openPeepOpenMenuItemIndex = [fileMenuItem.submenu indexOfItemWithTitle:@"Open Quickly…"] + 1;
+            [open setKeyEquivalentModifierMask:NSCommandKeyMask | NSControlKeyMask];
+            [open setTarget:self];
+            [[fileMenuItem submenu] insertItem:open atIndex:openPeepOpenMenuItemIndex];
+            [open release];
+        }
+    }
+    return self;
+}
+
+- (void)openWithPeepOpen
+{
+    NSString *open = @"/usr/bin/open";
+    NSArray *args = @[ @"-a", @"PeepOpen", self.projectRoot.path ];
+    
+    NSTask *task = [NSTask launchedTaskWithLaunchPath:open arguments:args];
+    [task waitUntilExit];
+}
+
+- (NSURL *)projectRoot
+{
+    Class IDEWorkspaceWindowController = NSClassFromString(@"IDEWorkspaceWindow");
+    NSURL *fileURL = (NSURL *)[[IDEWorkspaceWindowController lastActiveWorkspaceWindow] valueForKeyPath:@"windowController._workspace.representingFilePath._fileURL"];
+    
+    return [fileURL URLByDeletingLastPathComponent];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+@end
